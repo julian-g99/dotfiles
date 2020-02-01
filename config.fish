bd18db163c376c375cb7365467d13acb0f1c32a5
Julian Gu <j.gush99@gmail.com>
2020-02-01 17:08:41 -0500

    vi mod by default for fish

--- config.fish
+++ config.fish
@@ -28,4 +28,4 @@ export TERM=termite
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/julian/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
-
+fish_vi_key_bindings
