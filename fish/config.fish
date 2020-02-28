8a827f0542332f9b28d7fe726107be6648132cf4
Julian Gu <j.gush99@gmail.com>
2020-02-28 13:26:16 -0500

    fzf color scheme

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,29 @@
 set fish_greeting
 #base16-gruvbox-dark-medium
 base16-dracula
+set -l color00 '#282936'
+set -l color01 '#3a3c4e'
+set -l color02 '#626483'
+set -l color03 '#4d4f68'
+set -l color04 '#62d6e8'
+set -l color05 '#e9e9f4'
+set -l color06 '#f1f2f8'
+set -l color07 '#f7f7fb'
+set -l color08 '#ea51b2'
+set -l color09 '#b45bcf'
+set -l color0A '#ebff87'
+set -l color0B '#00f769'
+set -l color0C '#a1efe4'
+set -l color0D '#62d6e8'
+set -l color0E '#b45bcf'
+set -l color0F '#00f769'
+
+set -U FZF_DEFAULT_OPTS "
+  --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D
+  --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
+  --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
+"
+
 fish_vi_key_bindings
 
 function fish_user_key_bindings
